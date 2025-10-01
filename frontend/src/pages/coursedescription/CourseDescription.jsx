bff565c9c255262d9cc4d073c27eaad41668deaf
Harsh Doliya <137301163+CodeJoc@users.noreply.github.com>
2025-10-01 22:06:28 +0530

    Update

--- frontend/src/pages/coursedescription/CourseDescription.jsx
+++ frontend/src/pages/coursedescription/CourseDescription.jsx
@@ -39,10 +39,10 @@ const CourseDescription = ({ user }) => {
     );
 
     const options = {
-      key: "rzp_test_yOMeMyaj2wlvTt", // Enter the Key ID generated from the Dashboard
+      key: "rzp_test_ROAzQIiybRi8E8", // Enter the Key ID generated from the Dashboard
       amount: order.id, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency: "INR",
-      name: "E learning", //your business name
+      name: "EduNest", //your business name
       description: "Learn with us",
       order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
 
